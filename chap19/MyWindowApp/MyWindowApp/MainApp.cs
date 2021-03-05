d85c8200a55719e500ad81406daf3d91edd36a24
ieyeppo <ieyeppo@gmail.com>
2021-03-05 17:27:25 +0900

    ComboBox, CheckBox, TextBox, TrackBar, ProgressBar 사용 예제

---
+++ chap19/MyWindowApp/MyWindowApp/MainApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWindowApp
+{
+    class MainApp : Form
+    {
+        private string title;
+
+        public MainApp(string title)
+        {
+            this.title = title;
+            this.MouseClick += MainApp_MouseClick;
+        }
+
+        private void MainApp_MouseClick(object sender, MouseEventArgs e)
+        {
+            var result = $"sender : {sender}\r\n" +
+                         $"e: {e}\r\n" +
+                         $"e.X: {e.X}, e.Y : {e.Y}\r\n" +
+                         $"Button: {e.Button}, Clicks : {e.Clicks}\r\n";
+            MessageBox.Show(result);
+        }
+
+        static void Main()
+        {
+            Application.Run(new MainApp("Mouse Event App"));
+        }
+    }
+}
---
+++ chap19/UsingConstrolsApp/UsingConstrolsApp/FrmMain.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace UsingConstrolsApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.chkItalic = new System.Windows.Forms.CheckBox();
+            this.chkBold = new System.Windows.Forms.CheckBox();
+            this.cboFont = new System.Windows.Forms.ComboBox();
+            this.lblFont = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.trbHandle = new System.Windows.Forms.TrackBar();
+            this.prbDisplay = new System.Windows.Forms.ProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbHandle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtResult);
+            this.groupBox1.Controls.Add(this.chkItalic);
+            this.groupBox1.Controls.Add(this.chkBold);
+            this.groupBox1.Controls.Add(this.cboFont);
+            this.groupBox1.Controls.Add(this.lblFont);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(410, 93);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ComboBox, CheckBox, TextBox";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(24, 58);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(360, 21);
+            this.txtResult.TabIndex = 4;
+            // 
+            // chkItalic
+            // 
+            this.chkItalic.AutoSize = true;
+            this.chkItalic.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.chkItalic.Location = new System.Drawing.Point(324, 22);
+            this.chkItalic.Name = "chkItalic";
+            this.chkItalic.Size = new System.Drawing.Size(60, 16);
+            this.chkItalic.TabIndex = 3;
+            this.chkItalic.Text = "이텔릭";
+            this.chkItalic.UseVisualStyleBackColor = true;
+            this.chkItalic.CheckedChanged += new System.EventHandler(this.chkItalic_CheckedChanged);
+            // 
+            // chkBold
+            // 
+            this.chkBold.AutoSize = true;
+            this.chkBold.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.chkBold.Location = new System.Drawing.Point(270, 22);
+            this.chkBold.Name = "chkBold";
+            this.chkBold.Size = new System.Drawing.Size(50, 16);
+            this.chkBold.TabIndex = 2;
+            this.chkBold.Text = "굵게";
+            this.chkBold.UseVisualStyleBackColor = true;
+            // 
+            // cboFont
+            // 
+            this.cboFont.FormattingEnabled = true;
+            this.cboFont.Location = new System.Drawing.Point(69, 20);
+            this.cboFont.Name = "cboFont";
+            this.cboFont.Size = new System.Drawing.Size(193, 20);
+            this.cboFont.TabIndex = 1;
+            this.cboFont.Text = "폰트를 선택해주세요";
+            this.cboFont.SelectedIndexChanged += new System.EventHandler(this.cboFont_SelectedIndexChanged);
+            // 
+            // lblFont
+            // 
+            this.lblFont.AutoSize = true;
+            this.lblFont.Location = new System.Drawing.Point(22, 26);
+            this.lblFont.Name = "lblFont";
+            this.lblFont.Size = new System.Drawing.Size(41, 12);
+            this.lblFont.TabIndex = 0;
+            this.lblFont.Text = "Font : ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.prbDisplay);
+            this.groupBox2.Controls.Add(this.trbHandle);
+            this.groupBox2.Location = new System.Drawing.Point(13, 111);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(409, 106);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "TrackBar & ProgressBar";
+            // 
+            // trbHandle
+            // 
+            this.trbHandle.Location = new System.Drawing.Point(23, 21);
+            this.trbHandle.Maximum = 20;
+            this.trbHandle.Name = "trbHandle";
+            this.trbHandle.Size = new System.Drawing.Size(360, 45);
+            this.trbHandle.TabIndex = 0;
+            this.trbHandle.Scroll += new System.EventHandler(this.trbHandle_Scroll);
+            // 
+            // prbDisplay
+            // 
+            this.prbDisplay.Location = new System.Drawing.Point(23, 73);
+            this.prbDisplay.Maximum = 20;
+            this.prbDisplay.Name = "prbDisplay";
+            this.prbDisplay.Size = new System.Drawing.Size(360, 23);
+            this.prbDisplay.TabIndex = 1;
+            this.prbDisplay.Click += new System.EventHandler(this.prbDisplay_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 383);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Text = "Using Control App";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbHandle)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.CheckBox chkItalic;
+        private System.Windows.Forms.CheckBox chkBold;
+        private System.Windows.Forms.ComboBox cboFont;
+        private System.Windows.Forms.Label lblFont;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ProgressBar prbDisplay;
+        private System.Windows.Forms.TrackBar trbHandle;
+    }
+}
+
